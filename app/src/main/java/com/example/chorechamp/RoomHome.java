0574403ff80793337fc3a5c285796bd3f36c7c09
skulduggeryq <will.quarles426@gmail.com>
2023-11-28 13:42:32 -0600

    activity setup

---
+++ app/src/main/java/com/example/chorechamp/RoomHome.java
@@ -0,0 +1,14 @@
+package com.example.chorechamp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_home);
+    }
+}
